672ec371bd9c66bab23d843022d0639b153ee480
Sebastian Buergel <s.buergel@gmail.com>
2017-02-23 19:13:56 +0000

    added MIT license, added events and some minimal security checks

--- contracts/dgame.sol
+++ contracts/dgame.sol
@@ -17,7 +17,12 @@ contract dgame {
   uint registerDuration;
   uint endRegisterTime;
   address[] players;
+  mapping (address => bool) registered;
   string debug;
+  
+  event startedGame(address initiator, uint regTimeEnd, uint amountSent);
+  event registeredPlayer(address player);
+  event foundWinner(address player);
 
   // constructor sets default registration duration to 180s
   function dgame() {
@@ -26,22 +31,39 @@ contract dgame {
 
   // fallback function is used to register players and pay winner
   function () payable {
-    if (players.length == 0)
+    
+    // status idle: start new game and transition to status ongoing
+    if (endRegisterTime == 0) {
       endRegisterTime = now + registerDuration;
-    if (now > endRegisterTime && players.length > 0) {
+      if (msg.value == 0)
+        throw;  // prevent a new game to be started with empty pot
+      startedGame(msg.sender, endRegisterTime, msg.value);
+    }
+    
+    // status completed: find winner and transition to status idle
+    else if (now > endRegisterTime && players.length > 0) {
+      
       // find index of winner (take blockhash as source of entropy -> exploitable!)
       uint winner = uint(block.blockhash(block.number - 1)) % players.length;
-      
       // pay winner all Ether that we have
       // ignore if winner rejects prize
       // in that case Ether will be added to prize of the next game
       players[winner].send(this.balance);
-      
+      foundWinner(players[winner]);
+      endRegisterTime = 0; 
       // delete all players to allow for a next game
       delete players;
     }
-    else
+    
+    // status ongoing: register player
+    else {
+      if (msg.value != 0)
+        throw;  // prevent players from sending Ether to the game
+      if (registered[msg.sender])
+        throw;  // prevent same player to register twice with same address
+      registered[msg.sender] = true;
       players.push(msg.sender);
+      registeredPlayer(msg.sender);
+    }
   }
-  
 }

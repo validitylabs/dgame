6f866a4c6b3316e6df0a168ddcf2a518abb7709d
Sebastian C. Bürgel <SCBuergel@users.noreply.github.com>
2017-05-09 12:04:06 +0200

    fixed reset of players, added gameNumber, adhering to style guide

--- contracts/dgame.sol
+++ contracts/dgame.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.9;
+pragma solidity ^0.4.8;
 
 /*
  * This is an example gambling contract that works without any ABI interface.
@@ -14,56 +14,52 @@ pragma solidity ^0.4.9;
  */
 
 contract dgame {
-  uint registerDuration;
-  uint endRegisterTime;
-  address[] players;
-  mapping (address => bool) registered;
-  string debug;
-  
-  event startedGame(address initiator, uint regTimeEnd, uint amountSent);
-  event registeredPlayer(address player);
-  event foundWinner(address player);
+    uint public registerDuration;
+    uint public endRegisterTime;
+    uint public gameNumber;
+    uint public numPlayers;
+    mapping(uint => mapping(uint => address)) public players;
+    mapping(uint => mapping(address => bool)) public registered;
+    event StartedGame(address initiator, uint regTimeEnd, uint amountSent, uint gameNumber);
+    event RegisteredPlayer(address player, uint gameNumber);
+    event FoundWinner(address player, uint gameNumber);
+    
+    // constructor sets default registration duration to 5min
+    function dgame() {
+        registerDuration = 20;
+    }
+    
+    // fallback function is used for entire game logic
+    function() payable {
+        // status idle: start new game and transition to status ongoing
+        if (endRegisterTime == 0) {
+            endRegisterTime = now + registerDuration;
+            if (msg.value == 0)
+                throw;  // prevent a new game to be started with empty pot
+            StartedGame(msg.sender, endRegisterTime, msg.value, gameNumber);
+        } else if (now > endRegisterTime && numPlayers > 0) {
+            // status completed: find winner and transition to status idle
+            uint winner = uint(block.blockhash(block.number - 1)) % numPlayers; // find index of winner (take blockhash as source of entropy -> exploitable!)
+            uint currentGamenumber = gameNumber;
+            FoundWinner(players[currentGamenumber][winner], currentGamenumber);
+            endRegisterTime = 0;
+            numPlayers = 0;
+            gameNumber++;
 
-  // constructor sets default registration duration to 180s
-  function dgame() {
-    registerDuration = 300;
-  }
-
-  // fallback function is used to register players and pay winner
-  function () payable {
-    
-    // status idle: start new game and transition to status ongoing
-    if (endRegisterTime == 0) {
-      endRegisterTime = now + registerDuration;
-      if (msg.value == 0)
-        throw;  // prevent a new game to be started with empty pot
-      startedGame(msg.sender, endRegisterTime, msg.value);
+            // pay winner all Ether that we have
+            // ignore if winner rejects prize
+            // in that case Ether will be added to prize of the next game
+            players[currentGamenumber][winner].send(this.balance);
+        } else {
+            // status ongoing: register player
+            if (msg.value != 0)
+                throw;  // prevent players from sending Ether to the game
+            if (registered[gameNumber][msg.sender])
+                throw;  // prevent same player to register twice with same address
+            registered[gameNumber][msg.sender] = true;
+            players[gameNumber][numPlayers] = (msg.sender);
+            numPlayers++;
+            RegisteredPlayer(msg.sender, gameNumber);
+        }
     }
-    
-    // status completed: find winner and transition to status idle
-    else if (now > endRegisterTime && players.length > 0) {
-      
-      // find index of winner (take blockhash as source of entropy -> exploitable!)
-      uint winner = uint(block.blockhash(block.number - 1)) % players.length;
-      // pay winner all Ether that we have
-      // ignore if winner rejects prize
-      // in that case Ether will be added to prize of the next game
-      players[winner].send(this.balance);
-      foundWinner(players[winner]);
-      endRegisterTime = 0; 
-      // delete all players to allow for a next game
-      delete players;
-    }
-    
-    // status ongoing: register player
-    else {
-      if (msg.value != 0)
-        throw;  // prevent players from sending Ether to the game
-      if (registered[msg.sender])
-        throw;  // prevent same player to register twice with same address
-      registered[msg.sender] = true;
-      players.push(msg.sender);
-      registeredPlayer(msg.sender);
-    }
-  }
 }
